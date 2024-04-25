074605ed0d6722d5011ee35e8890ca5cb7681bc5
Riyan Dhiman <riyandhiman14@gmail.com>
2024-04-25 18:52:24 +0530

    Added idt isr

--- kernel/arch/i386/x86.c
+++ kernel/arch/i386/x86.c
@@ -4,7 +4,7 @@
 struct gdtdesc kgdt[5];
 struct gdtr kgdtr;
 struct idtr kidtr;
-struct idtdesc kidt[IDTSIZE];
+struct idtdesc kidt[1];
 
 void read_gdtr(uint32_t* tgdtr) {
     asm("sgdt %0" : "=m" (*tgdtr));
@@ -12,6 +12,10 @@ void read_gdtr(uint32_t* tgdtr) {
 
 void load_gdt(struct gdtr *gdtr) {
 	asm volatile("lgdt %0" : : "m" (*gdtr));
+}
+
+void load_idt(struct idtr *idtr) {
+	asm volatile("lidt %0" : : "m" (*idtr));
 }
 
 void gdt_init(void) {
@@ -50,8 +54,59 @@ void create_idt_descriptor(uint16_t select, uint32_t offset, uint16_t type, stru
 	return;
 }
 
+extern void _asm_int_0();
+extern void _asm_int_1();
+
+
+void outb(uint32_t ad, uint8_t v)
+{
+	asm volatile("outb %%al, %%dx" :: "d" (ad), "a" (v));
+}
+
+uint8_t inb(uint32_t ad)
+{
+	uint8_t v; 
+	asm volatile("inb %%dx, %%al" : "=a" (v) : "d" (ad)); 
+	return v;
+}
+
+void isr_default_int(int id)
+{
+	printf("default int %d", id);
+}
+
+void init_pic(void)
+{
+	outb(0x20, 0x11);
+	outb(0xA0, 0x11);
+
+	outb(0x21, 0x20);
+	outb(0xA1, 0x70);
+
+	outb(0x21, 0x04);
+	outb(0xA1, 0x02);
+
+	outb(0x21, 0x01);
+	outb(0xA1, 0x01);
+
+	outb(0x21, 0x0);
+	outb(0xA1, 0x0);
+}
 
 
 
 void init_idt(void) {
+	//int i;
+/*	for(i = 0; i < IDTSIZE; i++)
+	{
+		init_idt_descriptor(0x01, (uint32_t)_asm_schedule, INTGATE, &kidt[i]);
+	} */
+
+	create_idt_descriptor(0x08, (uint32_t) _asm_int_1, INTGATE, &kidt[0]);
+//	init_idt_descriptor(0x08, (uint32_t) _asm_init
+
+	kidtr.limit = sizeof(kidt) - 1;
+	kidtr.base = (uint32_t)&kidt;
+
+	load_idt(&kidtr);
 }
---
+++ kernel/arch/i386/x86_int.asm
@@ -0,0 +1,37 @@
+%macro SAVE_REGS 0
+	pushad
+	push ds
+	push es
+	push fs
+	push gs
+	push ebx
+	mov bx, 0x10
+	mov ds, bx
+	pop ebx
+%endmacro
+
+%macro RESTORE_REGS 0
+	pop gs
+	pop fs
+	pop es
+	pop ds
+	popad
+%endmacro
+
+%macro INTERRUPT 1
+global _asm_int_%1
+_asm_init_%1:
+	SAVE_REGS
+	push %1
+	call isr_default_int
+	pop eax
+	mov al, 0x20
+	out 0x20, al
+	RESTORE_REGS
+	iret
+%endmacro
+
+extern isr_default_int
+
+INTERRUPT 1
+INTERRUPT 2
